4ab1ee01d69587c6be2070531c1e4d7015de428a
alanroggers <alanroggersmp@outlook.com>
2023-06-06 23:35:43 -0600

    [Style] Importe un nuevo personaje con mejores animaciones

--- Assets/Scripts/Player/Animate.cs
+++ Assets/Scripts/Player/Animate.cs
@@ -4,23 +4,38 @@ using UnityEngine;
 
 public class Animate : MonoBehaviour
 {
-    private SpriteRenderer _spriteRenderer;
+    private bool _flipX;
     private Rigidbody2D _phys;
     private Animator _anim;
+    private Vector3 _left;
+    private Vector3 _right;
 
     void Awake()
     {
         _phys = GetComponent<Rigidbody2D>();
-        _spriteRenderer = GetComponent<SpriteRenderer>();
         _anim = GetComponent<Animator>();
+    }
+
+
+    void Start()
+    {
+        _left = new Vector3(-1,1,1);
+        _right = new Vector3(1,1,1);
     }
 
 
     void Update()
     {
         if (Mathf.Abs(_phys.velocity.x) > 0) {
-            _spriteRenderer.flipX = _phys.velocity.x < 0;
+            _flipX = _phys.velocity.x < 0;
             _anim.SetBool("Move", true);
         } else _anim.SetBool("Move", false);
+
+        if (_flipX)
+            transform.localScale = _left;
+        else
+            transform.localScale = _right;
+
+
     }
 }
